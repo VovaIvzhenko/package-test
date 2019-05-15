853308a1a87ccd0e834c13549a4a1b333077aae8
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-15 10:42:52 +0200

    Created StructureInterfaces

---
+++ app/interfaces/StructureInterfaces.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivzhe
+ * Date: 15.05.2019
+ * Time: 11:06
+ */
+
+namespace app\interfaces;
+
+
+/**
+ * Interface StructureInterfaces
+ * @package app\interfaces
+ */
+interface StructureInterfaces
+{
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function setProperties($data);
+}
